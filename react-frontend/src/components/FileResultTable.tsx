419282056b991a203da1eeb92e0e51e97e4f7b69
XUeshuo Li <spoonlee24k@gmail.com>
2024-07-20 16:50:12 +1000

    feat: added pagination for table

--- react-frontend/src/components/FileResultTable.tsx
+++ react-frontend/src/components/FileResultTable.tsx
@@ -6,8 +6,11 @@ import {
   TableCell,
   TableHead,
   TableRow,
+  TableFooter,
+  TablePagination,
 } from "@mui/material";
 import Scrollable from "./wrappers/Scrollable";
+import { TablePaginationActions } from "./TablePaginationActions";
 import { v4 as uuidv4 } from "uuid";
 
 import { Data } from "../interfaces/TableData";
@@ -24,12 +27,32 @@ const FileResultTable = ({
   // Extract the keys from the first object to dynamically create table headers
   const headers = tableData.length > 0 ? Object.keys(tableData[0]) : [];
 
-  console.log("tableData: ", tableData);
-  if (tableData.length > 0) {
-    console.log("tableData length bigger than 0");
-  }
+  // pagination hooks and handlers --------------------------------------------
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  // Avoid a layout jump when reaching the last page with empty rows.
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - tableData.length) : 0;
 
-  // const highlightColumn = "country";
+  const handleChangePage = (
+    event: React.MouseEvent<HTMLButtonElement> | null,
+    newPage: number
+  ) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  // jsx -----------------------------------------------------------------------
+  const tableDataToDisplay =
+    rowsPerPage > 0
+      ? tableData.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+      : tableData;
 
   return (
     <>
@@ -54,7 +77,7 @@ const FileResultTable = ({
               </TableRow>
             </TableHead>
             <TableBody>
-              {tableData.map((row, index) => (
+              {tableDataToDisplay.map((row, index) => (
                 // since we don't add or delete columns, we can safely use the index as the key
                 <TableRow key={index}>
                   {headers.map((header) => (
@@ -72,6 +95,28 @@ const FileResultTable = ({
                 </TableRow>
               ))}
             </TableBody>
+            <TableFooter>
+              <TableRow>
+                <TablePagination
+                  rowsPerPageOptions={[5, 10, 25, { label: "All", value: -1 }]}
+                  colSpan={3}
+                  count={tableData.length}
+                  rowsPerPage={rowsPerPage}
+                  page={page}
+                  slotProps={{
+                    select: {
+                      inputProps: {
+                        "aria-label": "rows per page",
+                      },
+                      native: true,
+                    },
+                  }}
+                  onPageChange={handleChangePage}
+                  onRowsPerPageChange={handleChangeRowsPerPage}
+                  ActionsComponent={TablePaginationActions}
+                />
+              </TableRow>
+            </TableFooter>
           </Table>
         </Scrollable>
       )}
